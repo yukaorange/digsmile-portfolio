9a8c6a3fffd45f14fcb1c8a5bebec4f1532f85a4
yukaorange <takaokar19911013t@gmail.com>
2022-04-20 11:53:08 +0900

    トップページ完成

--- js/main.js
+++ js/main.js
@@ -12,7 +12,29 @@ $(function () {
     }
   });
 });
+
 // リンククリックでメニューが閉じる
-$(".l-header_nav-item a[href]").on("click", function (event) {
-  $(".js-hamburger").trigger("click");
+$(".l-header_nav-item a[href],.l-header_nav-bg").on("click", function (event) {
+  $(".l-header_btn").trigger("click");
+});
+
+// スクロールするとヘッダーロゴの色変更
+$(window).on("scroll", function () {
+  const imageHeight = $(".scroll-checkPoint1").height();
+  if (imageHeight - 40 < $(this).scrollTop()) {
+    $(".l-header_logo").addClass("scrollColor");
+  } else {
+    $(".l-header_logo").removeClass("scrollColor");
+  }
+  // console.log(shadowHeight);
+});
+//スクロールするとスライドメニューボタンの変更
+$(window).on("scroll", function () {
+  const shadowHeight = $(".scroll-checkPoint2").height();
+  if (shadowHeight - 45 < $(this).scrollTop()) {
+    $(".l-header_btn").addClass("scrollColor");
+  } else {
+    $(".l-header_btn").removeClass("scrollColor");
+  }
+  // console.log(shadowHeight);
 });
